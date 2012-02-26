f474f3c401f27d95d1139d1632e3844263618209
Calvin <calvinxiu@gmail.com>
2012-02-26 23:42:23 +0800

    Unstable case in the slow ec2

--- modules/extension/src/test/java/org/springside/modules/memcached/SpyMemcachedClientTest.java
+++ modules/extension/src/test/java/org/springside/modules/memcached/SpyMemcachedClientTest.java
@@ -5,8 +5,8 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
-import org.springside.modules.memcached.SpyMemcachedClient;
 import org.springside.modules.test.spring.SpringContextTestCase;
+import org.springside.modules.utils.Threads;
 
 @ContextConfiguration(locations = { "/applicationContext-memcached.xml" })
 public class SpyMemcachedClientTest extends SpringContextTestCase {
@@ -21,11 +21,12 @@ public class SpyMemcachedClientTest extends SpringContextTestCase {
 		String value = "admin";
 
 		spyMemcachedClient.set(key, 60 * 60 * 1, value);
-
+		Threads.sleep(1000);
 		String result = spyMemcachedClient.get(key);
 		assertEquals(value, result);
 
 		spyMemcachedClient.delete(key);
+		Threads.sleep(1000);
 		result = spyMemcachedClient.get(key);
 		assertNull(result);
 	}
