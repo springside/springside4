7efc99860804ea351aa1ae8d8965da204d2233f1
Calvin <calvinxiu@gmail.com>
2012-03-19 16:31:36 +0800

    分开两种WebServiceClient的测试用例，第一种在缓慢的Jenkins跑不过去。

--- examples/mini-service/src/test/java/org/springside/examples/miniservice/functional/ws/AccountWebServiceWithPredefineClientIT.java
+++ examples/mini-service/src/test/java/org/springside/examples/miniservice/functional/ws/AccountWebServiceWithPredefineClientIT.java
@@ -2,40 +2,33 @@ package org.springside.examples.miniservice.functional.ws;
 
 import static org.junit.Assert.*;
 
-import javax.xml.ws.BindingProvider;
-
-import org.apache.cxf.jaxws.JaxWsProxyFactoryBean;
 import org.dozer.DozerBeanMapper;
 import org.junit.Test;
-import org.junit.experimental.categories.Category;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestExecutionListeners;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
-import org.springside.examples.miniservice.Start;
 import org.springside.examples.miniservice.data.AccountData;
 import org.springside.examples.miniservice.entity.User;
 import org.springside.examples.miniservice.functional.BaseFunctionalTestCase;
-import org.springside.examples.miniservice.functional.category.Smoke;
 import org.springside.examples.miniservice.webservice.dto.UserDTO;
 import org.springside.examples.miniservice.webservice.ws.AccountWebService;
-import org.springside.examples.miniservice.webservice.ws.result.UserListResult;
 import org.springside.examples.miniservice.webservice.ws.result.base.IdResult;
 import org.springside.examples.miniservice.webservice.ws.result.base.WSResult;
 
 /**
  * UserService Web服务的功能测试, 测试主要的接口调用.
  * 
- * 一般使用在Spring applicaitonContext.xml中用<jaxws:client/>创建的Client, 也可以用JAXWS的API自行创建.
+ * 使用在Spring applicaitonContext.xml中用<jaxws:client/>创建的Client.
  * 
  * @author calvin
  */
 @RunWith(SpringJUnit4ClassRunner.class)
 @TestExecutionListeners({ DependencyInjectionTestExecutionListener.class })
 @ContextConfiguration(locations = { "/applicationContext-ws-client.xml" })
-public class AccountWebServiceIT extends BaseFunctionalTestCase {
+public class AccountWebServiceWithPredefineClientIT extends BaseFunctionalTestCase {
 
 	@Autowired
 	private AccountWebService accountWebServiceClient;
@@ -74,28 +67,4 @@ public class AccountWebServiceIT extends BaseFunctionalTestCase {
 		result = accountWebServiceClient.createUser(userDTO);
 		assertEquals(result.getCode(), WSResult.PARAMETER_ERROR);
 	}
-
-	/**
-	 * 测试搜索用户,使用CXF的API自行动态创建Client.
-	 */
-	@Test
-	@Category(Smoke.class)
-	public void searchUser() {
-		String address = Start.TEST_BASE_URL + "/ws/accountservice";
-
-		JaxWsProxyFactoryBean proxyFactory = new JaxWsProxyFactoryBean();
-		proxyFactory.setAddress(address);
-		proxyFactory.setServiceClass(AccountWebService.class);
-		AccountWebService accountWebServiceCreated = (AccountWebService) proxyFactory.create();
-
-		//(可选)演示重新设定endpoint address.
-		((BindingProvider) accountWebServiceCreated).getRequestContext().put(BindingProvider.ENDPOINT_ADDRESS_PROPERTY,
-				address);
-
-		UserListResult result = accountWebServiceCreated.searchUser(null, null);
-
-		assertTrue(result.getUserList().size() >= 4);
-		assertEquals("Jack", result.getUserList().get(0).getName());
-	}
-
 }
