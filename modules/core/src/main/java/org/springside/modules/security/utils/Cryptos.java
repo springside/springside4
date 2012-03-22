9cfadab616e452d158b1b6d46f2173579032cfaf
Calvin <calvinxiu@gmail.com>
2012-03-22 12:08:49 +0800

    #23 加密模块支持IV(初始化向量)，让相同的原文加密出不同的密文。 另外输入的原文全部从String 改为 bytes, 不care encoding.

--- modules/core/src/main/java/org/springside/modules/security/utils/Cryptos.java
+++ modules/core/src/main/java/org/springside/modules/security/utils/Cryptos.java
@@ -6,14 +6,14 @@
 package org.springside.modules.security.utils;
 
 import java.security.GeneralSecurityException;
+import java.security.SecureRandom;
 import java.util.Arrays;
 
 import javax.crypto.Cipher;
 import javax.crypto.KeyGenerator;
 import javax.crypto.Mac;
 import javax.crypto.SecretKey;
-import javax.crypto.SecretKeyFactory;
-import javax.crypto.spec.DESKeySpec;
+import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
 
 import org.springside.modules.utils.Exceptions;
@@ -27,12 +27,15 @@ import org.springside.modules.utils.Exceptions;
  */
 public class Cryptos {
 
-	private static final String DES = "DES";
 	private static final String AES = "AES";
+	private static final String AES_CBC = "AES/CBC/PKCS5Padding";
 	private static final String HMACSHA1 = "HmacSHA1";
 
-	private static final int DEFAULT_HMACSHA1_KEYSIZE = 160;//RFC2401
+	private static final int DEFAULT_HMACSHA1_KEYSIZE = 160; //RFC2401
 	private static final int DEFAULT_AES_KEYSIZE = 128;
+	private static final int DEFAULT_IVSIZE = 16;
+
+	private static SecureRandom random = new SecureRandom();
 
 	private Cryptos() {
 	}
@@ -41,15 +44,15 @@ public class Cryptos {
 	/**
 	 * 使用HMAC-SHA1进行消息签名, 返回字节数组,长度为20字节.
 	 * 
-	 * @param input 原始输入字符串
-	 * @param keyBytes HMAC-SHA1密钥
+	 * @param input 原始输入字符数组
+	 * @param key HMAC-SHA1密钥
 	 */
-	public static byte[] hmacSha1(String input, byte[] keyBytes) {
+	public static byte[] hmacSha1(byte[] input, byte[] key) {
 		try {
-			SecretKey secretKey = new SecretKeySpec(keyBytes, HMACSHA1);
+			SecretKey secretKey = new SecretKeySpec(key, HMACSHA1);
 			Mac mac = Mac.getInstance(HMACSHA1);
 			mac.init(secretKey);
-			return mac.doFinal(input.getBytes());
+			return mac.doFinal(input);
 		} catch (GeneralSecurityException e) {
 			throw Exceptions.unchecked(e);
 		}
@@ -60,10 +63,10 @@ public class Cryptos {
 	 * 
 	 * @param expected 已存在的签名
 	 * @param input 原始输入字符串
-	 * @param keyBytes 密钥
+	 * @param key 密钥
 	 */
-	public static boolean isMacValid(byte[] expected, String input, byte[] keyBytes) {
-		byte[] actual = hmacSha1(input, keyBytes);
+	public static boolean isMacValid(byte[] expected, byte[] input, byte[] key) {
+		byte[] actual = hmacSha1(input, key);
 		return Arrays.equals(expected, actual);
 	}
 
@@ -82,113 +85,116 @@ public class Cryptos {
 		}
 	}
 
-	//-- DES function --//
-	/**
-	 * 使用DES加密原始字符串.
-	 * 
-	 * @param input 原始输入字符串
-	 * @param keyBytes 符合DES要求的密钥
-	 */
-	public static byte[] desEncrypt(String input, byte[] keyBytes) {
-		return des(input.getBytes(), keyBytes, Cipher.ENCRYPT_MODE);
-	}
-
-	/**
-	 * 使用DES解密加密的字符串, 返回原始字符串.
-	 * 
-	 * @param input Hex编码的加密字符串
-	 * @param keyBytes 符合DES要求的密钥
-	 */
-	public static String desDecrypt(byte[] input, byte[] keyBytes) {
-		byte[] decryptResult = des(input, keyBytes, Cipher.DECRYPT_MODE);
-		return new String(decryptResult);
-	}
-
-	/**
-	 * 使用DES加密或解密无编码的原始字节数组, 返回无编码的字节数组结果.
-	 * 
-	 * @param inputBytes 原始字节数组
-	 * @param keyBytes 符合DES要求的密钥
-	 * @param mode Cipher.ENCRYPT_MODE 或 Cipher.DECRYPT_MODE
-	 */
-	private static byte[] des(byte[] inputBytes, byte[] keyBytes, int mode) {
-		try {
-			DESKeySpec desKeySpec = new DESKeySpec(keyBytes);
-			SecretKeyFactory keyFactory = SecretKeyFactory.getInstance(DES);
-			SecretKey secretKey = keyFactory.generateSecret(desKeySpec);
-
-			Cipher cipher = Cipher.getInstance(DES);
-			cipher.init(mode, secretKey);
-			return cipher.doFinal(inputBytes);
-		} catch (GeneralSecurityException e) {
-			throw Exceptions.unchecked(e);
-		}
-	}
-
-	/**
-	 * 生成符合DES要求的密钥, 长度为64位(8字节).
-	 */
-	public static byte[] generateDesKey() {
-		try {
-			KeyGenerator keyGenerator = KeyGenerator.getInstance(DES);
-			SecretKey secretKey = keyGenerator.generateKey();
-			return secretKey.getEncoded();
-		} catch (GeneralSecurityException e) {
-			throw Exceptions.unchecked(e);
-		}
-	}
-
 	//-- AES funciton --//
 	/**
 	 * 使用AES加密原始字符串.
 	 * 
-	 * @param input 原始输入字符串
-	 * @param keyBytes 符合AES要求的密钥
+	 * @param input 原始输入字符数组
+	 * @param key 符合AES要求的密钥
 	 */
-	public static byte[] aesEncrypt(String input, byte[] keyBytes) {
-		return aes(input.getBytes(), keyBytes, Cipher.ENCRYPT_MODE);
+	public static byte[] aesEncrypt(byte[] input, byte[] key) {
+		return aes(input, key, Cipher.ENCRYPT_MODE);
+	}
+
+	/**
+	 * 使用AES加密原始字符串.
+	 * 
+	 * @param input 原始输入字符数组
+	 * @param key 符合AES要求的密钥
+	 * @param iv 初始向量
+	 */
+	public static byte[] aesEncrypt(byte[] input, byte[] key, byte[] iv) {
+		return aes(input, key, iv, Cipher.ENCRYPT_MODE);
 	}
 
 	/**
 	 * 使用AES解密字符串, 返回原始字符串.
 	 * 
 	 * @param input Hex编码的加密字符串
-	 * @param keyBytes 符合AES要求的密钥
+	 * @param key 符合AES要求的密钥
 	 */
-	public static String aesDecrypt(byte[] input, byte[] keyBytes) {
-		byte[] decryptResult = aes(input, keyBytes, Cipher.DECRYPT_MODE);
+	public static String aesDecrypt(byte[] input, byte[] key) {
+		byte[] decryptResult = aes(input, key, Cipher.DECRYPT_MODE);
+		return new String(decryptResult);
+	}
+
+	/**
+	 * 使用AES解密字符串, 返回原始字符串.
+	 * 
+	 * @param input Hex编码的加密字符串
+	 * @param key 符合AES要求的密钥
+	 * @param iv 初始向量
+	 */
+	public static String aesDecrypt(byte[] input, byte[] key, byte[] iv) {
+		byte[] decryptResult = aes(input, key, iv, Cipher.DECRYPT_MODE);
 		return new String(decryptResult);
 	}
 
 	/**
 	 * 使用AES加密或解密无编码的原始字节数组, 返回无编码的字节数组结果.
 	 * 
-	 * @param inputBytes 原始字节数组
-	 * @param keyBytes 符合AES要求的密钥
+	 * @param input 原始字节数组
+	 * @param key 符合AES要求的密钥
 	 * @param mode Cipher.ENCRYPT_MODE 或 Cipher.DECRYPT_MODE
 	 */
-	private static byte[] aes(byte[] inputBytes, byte[] keyBytes, int mode) {
+	private static byte[] aes(byte[] input, byte[] key, int mode) {
 		try {
-			SecretKey secretKey = new SecretKeySpec(keyBytes, AES);
+			SecretKey secretKey = new SecretKeySpec(key, AES);
 			Cipher cipher = Cipher.getInstance(AES);
 			cipher.init(mode, secretKey);
-			return cipher.doFinal(inputBytes);
+			return cipher.doFinal(input);
 		} catch (GeneralSecurityException e) {
 			throw Exceptions.unchecked(e);
 		}
 	}
 
 	/**
-	 * 生成AES密钥,返回字节数组,长度为128位(16字节).
+	 * 使用AES加密或解密无编码的原始字节数组, 返回无编码的字节数组结果.
+	 * 
+	 * @param input 原始字节数组
+	 * @param key 符合AES要求的密钥
+	 * @param iv 初始向量
+	 * @param mode Cipher.ENCRYPT_MODE 或 Cipher.DECRYPT_MODE
+	 */
+	private static byte[] aes(byte[] input, byte[] key, byte[] iv, int mode) {
+		try {
+			SecretKey secretKey = new SecretKeySpec(key, AES);
+			IvParameterSpec ivSpec = new IvParameterSpec(iv);
+			Cipher cipher = Cipher.getInstance(AES_CBC);
+			cipher.init(mode, secretKey, ivSpec);
+			return cipher.doFinal(input);
+		} catch (GeneralSecurityException e) {
+			throw Exceptions.unchecked(e);
+		}
+	}
+
+	/**
+	 * 生成AES密钥,返回字节数组, 默认长度为128位(16字节).
 	 */
 	public static byte[] generateAesKey() {
+		return generateAesKey(DEFAULT_AES_KEYSIZE);
+	}
+
+	/**
+	 * 生成AES密钥,可选长度为128,192,256位.
+	 */
+	public static byte[] generateAesKey(int keysize) {
 		try {
 			KeyGenerator keyGenerator = KeyGenerator.getInstance(AES);
-			keyGenerator.init(DEFAULT_AES_KEYSIZE);
+			keyGenerator.init(keysize);
 			SecretKey secretKey = keyGenerator.generateKey();
 			return secretKey.getEncoded();
 		} catch (GeneralSecurityException e) {
 			throw Exceptions.unchecked(e);
 		}
+	}
+
+	/**
+	 * 生成随机向量,默认大小为cipher.getBlockSize(), 16字节.
+	 */
+	public static byte[] generateIV() {
+		byte[] bytes = new byte[DEFAULT_IVSIZE];
+		random.nextBytes(bytes);
+		return bytes;
 	}
 }
--- modules/core/src/test/java/org/springside/modules/security/utils/DigestsTest.java
+++ modules/core/src/test/java/org/springside/modules/security/utils/DigestsTest.java
@@ -12,15 +12,15 @@ public class DigestsTest {
 	@Test
 	public void digestString() {
 		String input = "user";
-		byte[] sha1Result = Digests.sha1(input);
+		byte[] sha1Result = Digests.sha1(input.getBytes());
 		System.out.println("sha1 in hex result                               :" + Encodes.encodeHex(sha1Result));
 
 		byte[] salt = Digests.generateSalt(8);
 		System.out.println("salt in hex                                      :" + Encodes.encodeHex(salt));
-		sha1Result = Digests.sha1(input, salt);
+		sha1Result = Digests.sha1(input.getBytes(), salt);
 		System.out.println("sha1 in hex result with salt                     :" + Encodes.encodeHex(sha1Result));
 
-		sha1Result = Digests.sha1(input, salt, 1024);
+		sha1Result = Digests.sha1(input.getBytes(), salt, 1024);
 		System.out.println("sha1 in hex result with salt and 1024 interations:" + Encodes.encodeHex(sha1Result));
 
 	}
