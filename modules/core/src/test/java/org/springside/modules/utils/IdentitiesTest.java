29168f8827cb770e549c01029bdb1c73f10b6ceb
Calvin <calvinxiu@gmail.com>
2012-03-27 22:53:40 +0800

    cleanup

--- modules/core/src/test/java/org/springside/modules/utils/IdentitiesTest.java
+++ modules/core/src/test/java/org/springside/modules/utils/IdentitiesTest.java
@@ -9,7 +9,7 @@ public class IdentitiesTest {
 		System.out.println("uuid: " + Identities.uuid());
 		System.out.println("uuid2:" + Identities.uuid2());
 		System.out.println("randomLong:  " + Identities.randomLong());
-		System.out.println("randomBase62:" + Identities.randomBase62(6));
+		System.out.println("randomBase62:" + Identities.randomBase62(7));
 	}
 
 }
