f1b360f05d99181ad689a35eb62737659af7d5d8
hzl7652 <hzl7652@sina.com>
2012-03-28 19:35:54 +0800

    增加  CacheBuilder removalListener 演示 增加  Cache.get(K key, Callable<? extends V> valueLoader) 演示

--- examples/showcase/src/main/java/org/springside/examples/showcase/cache/guava/GuavaCacheDemo.java
+++ examples/showcase/src/main/java/org/springside/examples/showcase/cache/guava/GuavaCacheDemo.java
@@ -2,10 +2,13 @@ package org.springside.examples.showcase.cache.guava;
 
 import static org.junit.Assert.*;
 
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
 
 import javax.annotation.Resource;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.transaction.TransactionConfiguration;
@@ -14,9 +17,12 @@ import org.springside.examples.showcase.common.service.AccountManager;
 import org.springside.modules.test.data.H2Fixtures;
 import org.springside.modules.test.spring.SpringTxTestCase;
 
+import com.google.common.cache.Cache;
 import com.google.common.cache.CacheBuilder;
 import com.google.common.cache.CacheLoader;
 import com.google.common.cache.LoadingCache;
+import com.google.common.cache.RemovalListener;
+import com.google.common.cache.RemovalNotification;
 
 /**
  * 本地缓存演示，使用GuavaCache.
@@ -26,15 +32,33 @@ import com.google.common.cache.LoadingCache;
 @TransactionConfiguration(transactionManager = "defaultTransactionManager")
 public class GuavaCacheDemo extends SpringTxTestCase {
 
-	@Resource
+	private LoadingCache<Long, User> userCacheFormCacheLoader;
+
+	private Cache<Long, User> userCacheFormCallable = CacheBuilder.newBuilder().maximumSize(100).build();
+
 	private AccountManager accountManager;
 
-	@Test
-	public void test1() throws Exception {
-		H2Fixtures.reloadAllTable(dataSource, "/data/sample-data.xml");
-		LoadingCache<Long, User> cache = CacheBuilder.newBuilder().maximumSize(100)//设置缓存个数
+	@Resource
+	public void setAccountManager(AccountManager accountManager) {
+		this.accountManager = accountManager;
+		initUserCacheFormCacheLoader();
+	}
+
+	/**
+	 * 初始化缓存.
+	 */
+	private void initUserCacheFormCacheLoader() {
+		userCacheFormCacheLoader = CacheBuilder.newBuilder().maximumSize(100)//设置缓存个数
 				.expireAfterAccess(3, TimeUnit.SECONDS)//缓存过期时间为8秒
-				.build(new CacheLoader<Long, User>() {
+				.removalListener(new RemovalListener<Long, User>() {//增加移除监听器
+
+							@Override
+							public void onRemoval(RemovalNotification<Long, User> notification) {
+								System.out.println("用户id为:" + notification.getKey() + "被移除");
+
+							}
+
+						}).build(new CacheLoader<Long, User>() {
 
 					@Override
 					public User load(Long key) throws Exception {
@@ -42,17 +66,57 @@ public class GuavaCacheDemo extends SpringTxTestCase {
 					}
 
 				});
+	}
 
-		User user = cache.get(1L);
-		assertEquals("admin", user.getLoginName());//第一次加载会查数据库
+	@Before
+	public void before() throws Exception {
+		H2Fixtures.reloadAllTable(dataSource, "/data/sample-data.xml");
 
-		User user2 = cache.get(1L);//第二次加载时直接从缓存里取
+	}
+
+	/**
+	 * 演示从CacheLoader加载数据.
+	 */
+	@Test
+	public void testFormCacheLoader() throws Exception {
+		User user = userCacheFormCacheLoader.get(1L);//第一次加载会查数据库
+		assertEquals("admin", user.getLoginName());
+
+		User user2 = userCacheFormCacheLoader.get(1L);//第二次加载时直接从缓存里取
 		assertEquals("admin", user2.getLoginName());
-
-		Thread.sleep(10000);
-		user = cache.get(1L);
-		;//第三次加载时，因为缓存已经过期所以会查数据库
+		assertSame(user, user2);
+		Thread.sleep(8000);
+		user = userCacheFormCacheLoader.get(1L);
+		//第三次加载时，因为缓存已经过期所以会查数据库
 		System.out.println(user);
 		assertEquals("admin", user.getLoginName());
+	}
+
+	/**
+	 * 演示从Callable加载数据.
+	 */
+	@Test
+	public void testFormCallable() {
+		final long userId = 1L;
+		User user = getUserFromCallableCache(userId);//第一次加载会查数据库
+		assertEquals("admin", user.getLoginName());
+		User user2 = getUserFromCallableCache(userId);//第二次加载时直接从缓存里取
+		assertEquals("admin", user2.getLoginName());
+	}
+
+	private User getUserFromCallableCache(final Long userId) {
+		try {
+			//Callable只有在缓存值不存在时，才会调用
+			return userCacheFormCallable.get(userId, new Callable<User>() {
+				@Override
+				public User call() throws Exception {
+					return accountManager.getUser(userId);
+				}
+			});
+		} catch (ExecutionException e) {
+			e.printStackTrace();
+			return null;
+		}
+
 	}
 }
