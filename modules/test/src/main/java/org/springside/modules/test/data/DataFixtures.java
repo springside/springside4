81decc321cc3463f206cd3b7c06434fa527d679d
Calvin <calvinxiu@gmail.com>
2012-08-24 23:42:13 +0800

    fix classpath

--- modules/test/src/main/java/org/springside/modules/test/data/DataFixtures.java
+++ modules/test/src/main/java/org/springside/modules/test/data/DataFixtures.java
@@ -3,9 +3,7 @@ package org.springside.modules.test.data;
 import javax.sql.DataSource;
 
 import org.springframework.core.io.DefaultResourceLoader;
-import org.springframework.core.io.Resource;
 import org.springframework.core.io.ResourceLoader;
-import org.springframework.core.io.support.EncodedResource;
 import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.simple.SimpleJdbcTemplate;
 import org.springframework.test.jdbc.SimpleJdbcTestUtils;
@@ -18,8 +16,7 @@ public class DataFixtures {
 		SimpleJdbcTemplate simpleJdbcTemplate = new SimpleJdbcTemplate(dataSource);
 
 		for (String sqlResourcePath : sqlResourcePaths) {
-			Resource resource = resourceLoader.getResource(sqlResourcePath);
-			SimpleJdbcTestUtils.executeSqlScript(simpleJdbcTemplate, new EncodedResource(resource, null), true);
+			SimpleJdbcTestUtils.executeSqlScript(simpleJdbcTemplate, resourceLoader, sqlResourcePath, true);
 		}
 	}
 }
