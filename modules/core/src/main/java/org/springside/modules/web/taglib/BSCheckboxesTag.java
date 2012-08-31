14bbdd09c0c47968b9b8a137bd6f0abe4ae66725
Calvin <calvinxiu@gmail.com>
2012-09-01 02:03:07 +0800

    #85 spring mvc taglib的checkbox, radio 支持twitter bootstrap

---
+++ modules/core/src/main/java/org/springside/modules/web/taglib/BSCheckboxesTag.java
@@ -0,0 +1,31 @@
+package org.springside.modules.web.taglib;
+
+import javax.servlet.jsp.JspException;
+
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.servlet.tags.form.TagWriter;
+
+public class BSCheckboxesTag extends BSAbstractMultiCheckedElementTag {
+
+	@Override
+	protected int writeTagContent(TagWriter tagWriter) throws JspException {
+		super.writeTagContent(tagWriter);
+
+		if (!isDisabled()) {
+			// Write out the 'field was present' marker.
+			tagWriter.startTag("input");
+			tagWriter.writeAttribute("type", "hidden");
+			String name = WebDataBinder.DEFAULT_FIELD_MARKER_PREFIX + getName();
+			tagWriter.writeAttribute("name", name);
+			tagWriter.writeAttribute("value", processFieldValue(name, "on", getInputType()));
+			tagWriter.endTag();
+		}
+
+		return SKIP_BODY;
+	}
+
+	@Override
+	protected String getInputType() {
+		return "checkbox";
+	}
+}
