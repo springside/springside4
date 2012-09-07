e6d1dd2764dbe7594300931bc002ce386f2cc7fa
Calvin <calvinxiu@gmail.com>
2012-09-08 02:04:52 +0800

    #60 H2 + Oracle的实际开发场景

---
+++ examples/quickstart/src/main/resources/sql/oracle/schema.sql
@@ -0,0 +1,24 @@
+drop table ss_task;
+drop table ss_user;
+
+create table ss_task (
+	id number(19,0),
+	title varchar2(128) not null,
+	description varchar2(255),
+	user_id bigint,
+    primary key (id)
+);
+
+create table ss_user (
+	id number(19,0),
+	login_name varchar2(64) not null unique,
+	name varchar2(64),
+	password varchar2(255),
+	salt varchar2(64),
+	roles varchar2(255),
+	primary key (id)
+);
+
+
+create sequence ss_seq_task start with 100 increment by 20;
+create sequence ss_seq_user start with 100 increment by 20;
