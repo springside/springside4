dffc9dbb10c55c97a86b17b746859f312369de63
Calvin <calvinxiu@gmail.com>
2013-07-10 01:34:36 +0800

    fix testcase

--- examples/quickstart/src/main/java/org/springside/examples/quickstart/rest/RestExceptionHandler.java
+++ examples/quickstart/src/main/java/org/springside/examples/quickstart/rest/RestExceptionHandler.java
@@ -1,5 +1,7 @@
 package org.springside.examples.quickstart.rest;
 
+import java.util.Map;
+
 import javax.validation.ConstraintViolationException;
 
 import org.springframework.http.HttpHeaders;
@@ -11,13 +13,16 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 import org.springside.modules.beanvalidator.BeanValidators;
+import org.springside.modules.mapper.JsonMapper;
 
 @ControllerAdvice
 public class RestExceptionHandler extends ResponseEntityExceptionHandler {
+	private JsonMapper jsonMapper = new JsonMapper();
 
 	@ExceptionHandler(value = { ConstraintViolationException.class })
 	public final ResponseEntity<?> handleException(ConstraintViolationException ex, WebRequest request) {
-		String body = BeanValidators.extractPropertyAndMessage(ex.getConstraintViolations()).toString();
+		Map<String, String> errors = BeanValidators.extractPropertyAndMessage(ex.getConstraintViolations());
+		String body = jsonMapper.toJson(errors);
 		HttpHeaders headers = new HttpHeaders();
 		headers.setContentType(MediaType.TEXT_PLAIN);
 		return handleExceptionInternal(ex, body, null, HttpStatus.BAD_REQUEST, request);
