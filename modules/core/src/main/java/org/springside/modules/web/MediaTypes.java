eecea81dc7db52197a1aaf182a72db6e2141ef0d
Calvin <calvinxiu@gmail.com>
2013-07-24 00:38:25 +0800

    现有的MediaType没有UTF-8 charset的定义，只好自己重新定义

--- modules/core/src/main/java/org/springside/modules/web/MediaTypes.java
+++ modules/core/src/main/java/org/springside/modules/web/MediaTypes.java
@@ -1,7 +1,10 @@
 package org.springside.modules.web;
 
 /**
- * 带UTF8 charset 定义的MediaType
+ * 带UTF-8 charset 定义的MediaType.
+ * 
+ * Jax-RS和Spring的MediaType没有UTF-8的版本，
+ * Google的MediaType必须再调用toString()函数而不是常量，不能用于Restful方法的annotation。
  * 
  * @author calvin
  */
