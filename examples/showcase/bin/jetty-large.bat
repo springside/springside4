4b91c9508f3f46296bc45fc3a0bad615326b20c9
Calvin <calvinxiu@gmail.com>
2013-07-27 18:14:35 +0800

    #238 使用JMeter对Showcase进行性能测试，变量可在properties文件里定义

--- examples/showcase/bin/jetty-large.bat
+++ examples/showcase/bin/jetty-large.bat
@@ -4,7 +4,7 @@ echo [INFO] Use maven jetty-plugin run the project.
 cd %~dp0
 cd ..
 
-set MAVEN_OPTS=%MAVEN_OPTS% -XX:MaxPermSize=128m -Xms1024m -Xmx1024m -XX:NewSize=512m -XX:MaxNewSize=512m
+set MAVEN_OPTS=%MAVEN_OPTS% -XX:MaxPermSize=128m -Xms1024m -Xmx1024m -XX:NewSize=512m -server
 call mvn jetty:run -Djetty.port=8080
 
 cd bin
