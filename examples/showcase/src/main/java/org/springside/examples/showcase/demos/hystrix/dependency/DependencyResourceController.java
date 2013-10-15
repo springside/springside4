0e1b825699276693e8b6105c7fe76d4394e96b26
Calvin <calvinxiu@gmail.com>
2013-10-15 18:19:15 +0800

    add hystrix demo back.

---
+++ examples/showcase/src/main/java/org/springside/examples/showcase/demos/hystrix/dependency/DependencyResourceController.java
@@ -0,0 +1,74 @@
+package org.springside.examples.showcase.demos.hystrix.dependency;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springside.examples.showcase.entity.User;
+import org.springside.examples.showcase.service.AccountEffectiveService;
+import org.springside.examples.showcase.service.ServiceException;
+import org.springside.examples.showcase.webservice.rest.RestException;
+import org.springside.examples.showcase.webservice.rest.UserDTO;
+import org.springside.modules.mapper.BeanMapper;
+import org.springside.modules.utils.Threads;
+
+/**
+ * 模拟被Service所依赖的远程Resource.
+ */
+@Controller
+public class DependencyResourceController {
+	public static final int TIMEOUT = 10000;
+
+	private String status = "normal";
+
+	@Autowired
+	private AccountEffectiveService accountService;
+
+	/**
+	 * 根据控制器中的状态而演示不同的行为，如正常返回，10秒后返回或直接报错
+	 */
+	@RequestMapping(value = "/hystrix/resource/{id}", method = RequestMethod.GET)
+	@ResponseBody
+	public UserDTO getUser(@PathVariable("id") Long id) {
+		// 正常返回.
+		if ("normal".equals(status)) {
+			return handleRequest(id);
+		}
+
+		// 演示超时，10秒后返回.
+		if ("timeout".equals(status)) {
+			Threads.sleep(TIMEOUT);
+			return handleRequest(id);
+		}
+
+		// 演示服务端出错情况.
+		if ("fail".equals(status)) {
+			throw new ServiceException("Server Exception");
+		}
+
+		// 演示客户端请求出错。
+		if ("clientfail".equals(status)) {
+			throw new RestException(HttpStatus.BAD_REQUEST, "Client Exception");
+		}
+		return null;
+	}
+
+	private UserDTO handleRequest(Long id) {
+		User user = accountService.getUser(id);
+		UserDTO dto = BeanMapper.map(user, UserDTO.class);
+		dto.setTeamId(user.getTeam().getId());
+		return dto;
+	}
+
+	/**
+	 * 设定Resource的状态.
+	 */
+	@RequestMapping(value = "/hystrix/resource/status/{status}")
+	public String updateStatus(@PathVariable("status") String newStatus) {
+		status = newStatus;
+		return "redirect:/story/hystrix";
+	}
+}
