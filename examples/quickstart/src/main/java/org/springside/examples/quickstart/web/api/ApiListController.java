5b72ac3091f5d973befd4442a7d7feed0202e02c
Calvin <calvinxiu@gmail.com>
2013-11-21 11:34:37 +0800

    Quickstart 增加API 列表页面

---
+++ examples/quickstart/src/main/java/org/springside/examples/quickstart/web/api/ApiListController.java
@@ -0,0 +1,14 @@
+package org.springside.examples.quickstart.web.api;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping(value = "/api")
+public class ApiListController {
+	@RequestMapping(method = RequestMethod.GET)
+	public String list() {
+		return "api/list";
+	}
+}
