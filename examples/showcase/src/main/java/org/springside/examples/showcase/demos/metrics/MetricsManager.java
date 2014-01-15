849506e1299a8bb663443e80a54688a21b83574b
Calvin <calvinxiu@gmail.com>
2014-01-16 00:36:43 +0800

    #322 默认不打开GraphiteReporter

--- examples/showcase/src/main/java/org/springside/examples/showcase/demos/metrics/MetricsManager.java
+++ examples/showcase/src/main/java/org/springside/examples/showcase/demos/metrics/MetricsManager.java
@@ -15,16 +15,27 @@ public class MetricsManager {
 
 	private ReportScheduler scheduler;
 
+	private boolean graphiteEnabled = false;
+
 	@PostConstruct
 	public void start() {
-		GraphiteReporter graphiteReporter = new GraphiteReporter(new InetSocketAddress("localhost", 2003));
 		ConsoleReporter consoleReporter = new ConsoleReporter();
-		scheduler = new ReportScheduler(MetricRegistry.INSTANCE, consoleReporter, graphiteReporter);
+		scheduler = new ReportScheduler(MetricRegistry.INSTANCE, consoleReporter);
+
+		if (graphiteEnabled) {
+			GraphiteReporter graphiteReporter = new GraphiteReporter(new InetSocketAddress("localhost", 2003));
+			scheduler.addReporter(graphiteReporter);
+		}
+
 		scheduler.start(10, TimeUnit.SECONDS);
 	}
 
 	@PreDestroy
 	public void stop() {
 		scheduler.stop();
+	}
+
+	public void setGraphiteEnabled(boolean graphiteEnabled) {
+		this.graphiteEnabled = graphiteEnabled;
 	}
 }
