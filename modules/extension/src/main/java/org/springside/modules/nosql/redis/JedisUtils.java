64c14e58b2c4242596ffb55686b5504814b4b708
Calvin <calvinxiu@gmail.com>
2014-02-04 15:40:15 +0800

    update 3pp version, spring 4.0->4.0.1(bug fixing), jedis 2.2.1->2.3.0(apache connection pool from 1.5->2.0,support SCAN and WAIT), logback 1.0.3->1.1.0(bug fixing), powermock 1.5.3->1.5.4, ehcache 2.6.6->2.6.8, cxf 2.7.8->2.7.9,

--- modules/extension/src/main/java/org/springside/modules/nosql/redis/JedisUtils.java
+++ modules/extension/src/main/java/org/springside/modules/nosql/redis/JedisUtils.java
@@ -18,32 +18,34 @@ public class JedisUtils {
 	private static final String OK_MULTI_CODE = "+OK";
 
 	/**
-	 * 判断 是 OK 或 +OK.
-	 */
-	public static boolean isStatusOk(String status) {
-		return (status != null) && (OK_CODE.equals(status) || OK_MULTI_CODE.equals(status));
-	}
-
-	/**
 	 * 快速设置JedisPoolConfig, 不执行idle checking。
 	 */
-	public static JedisPoolConfig createPoolConfig(int maxIdle, int maxActive) {
+	public static JedisPoolConfig createPoolConfig(int maxIdle, int maxTotal) {
 		JedisPoolConfig poolConfig = new JedisPoolConfig();
 		poolConfig.setMaxIdle(maxIdle);
-		poolConfig.setMaxActive(maxActive);
+		poolConfig.setMaxTotal(maxTotal);
 		poolConfig.setTimeBetweenEvictionRunsMillis(-1);
 		return poolConfig;
 	}
 
 	/**
-	 * 快速设置JedisPoolConfig, 设置执行idle checking的间隔和可清除的idle时间.
+	 * 快速设置JedisPoolConfig, 设置执行idle checking的间隔和可被清除的idle时间.
+	 * 默认的checkingIntervalSecs是30秒，可被清除时间是60秒。
 	 */
-	public static JedisPoolConfig createPoolConfig(int maxIdle, int maxActive, int checkingIntervalSecs,
+	public static JedisPoolConfig createPoolConfig(int maxIdle, int maxTotal, int checkingIntervalSecs,
 			int evictableIdleTimeSecs) {
-		JedisPoolConfig poolConfig = createPoolConfig(maxIdle, maxActive);
+		JedisPoolConfig poolConfig = createPoolConfig(maxIdle, maxTotal);
+
 		poolConfig.setTimeBetweenEvictionRunsMillis(checkingIntervalSecs * 1000);
 		poolConfig.setMinEvictableIdleTimeMillis(evictableIdleTimeSecs * 1000);
 		return poolConfig;
+	}
+
+	/**
+	 * 判断 是 OK 或 +OK.
+	 */
+	public static boolean isStatusOk(String status) {
+		return (status != null) && (OK_CODE.equals(status) || OK_MULTI_CODE.equals(status));
 	}
 
 	/**
