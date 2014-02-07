914300f12e1367bc18f1882909b7d796b82cb03a
Calvin <calvinxiu@gmail.com>
2014-02-07 21:30:27 +0800

    typo

--- examples/showcase/bin/yuicompressor.bat
+++ examples/showcase/bin/yuicompressor.bat
@@ -1,5 +1,5 @@
 @echo off
-echo [PreRequirment] Download lasteds version of yuicompressor from https://github.com/yui/yuicompressor/releases and put it here.
+echo [PreRequirment] Download last version of yuicompressor from https://github.com/yui/yuicompressor/releases and put it here.
 
 cd %~dp0
 java -jar yuicompressor-*.jar -o ..\src\main\webapp\static\styles\default.min.css ..\src\main\webapp\static\styles\default.css
