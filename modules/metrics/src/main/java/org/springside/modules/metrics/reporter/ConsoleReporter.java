5e0ae65995165741dcff0c5e23fc81b8dc78e92f
Jenning Xu <jenning.xu@ericsson.com>
2014-11-21 14:31:07 +0800

    [metrics]fix lastRate & meanRate long/double problem

--- modules/metrics/src/main/java/org/springside/modules/metrics/reporter/ConsoleReporter.java
+++ modules/metrics/src/main/java/org/springside/modules/metrics/reporter/ConsoleReporter.java
@@ -83,8 +83,8 @@ public class ConsoleReporter implements Reporter {
 	private void printCounter(CounterMetric counter) {
 		output.printf("        last count = %d%n", counter.lastCount);
 		output.printf("       total count = %d%n", counter.totalCount);
-		output.printf("         last rate = %2.2f/s%n", counter.lastRate);
-		output.printf("         mean rate = %2.2f/s%n", counter.meanRate);
+		output.printf("         last rate = %d%n", counter.lastRate);
+		output.printf("         mean rate = %d%n", counter.meanRate);
 	}
 
 	private void printHistogram(HistogramMetric histogram) {
