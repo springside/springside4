25d1855d379113c382724b1b0fa6fd1d27d23ec5
kangxh <kangxh@lordstar.net>
2014-12-09 10:00:09 +0800

    添加时间格式的filter

--- modules/core/src/main/java/org/springside/modules/persistence/SearchFilter.java
+++ modules/core/src/main/java/org/springside/modules/persistence/SearchFilter.java
@@ -5,6 +5,8 @@
  *******************************************************************************/
 package org.springside.modules.persistence;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Map;
 import java.util.Map.Entry;
 
@@ -12,49 +14,67 @@ import org.apache.commons.lang3.StringUtils;
 
 import com.google.common.collect.Maps;
 
+import static org.springside.modules.persistence.SearchFilter.Type.DATETIME;
+
 public class SearchFilter {
 
-	public enum Operator {
-		EQ, LIKE, GT, LT, GTE, LTE
-	}
+    public enum Operator {
+        EQ, LIKE, GT, LT, GTE, LTE
+    }
 
-	public String fieldName;
-	public Object value;
-	public Operator operator;
+    public enum Type {
+        DATETIME
+    }
 
-	public SearchFilter(String fieldName, Operator operator, Object value) {
-		this.fieldName = fieldName;
-		this.value = value;
-		this.operator = operator;
-	}
+    public String fieldName;
+    public Object value;
+    public Operator operator;
 
-	/**
-	 * searchParams中key的格式为OPERATOR_FIELDNAME
-	 */
-	public static Map<String, SearchFilter> parse(Map<String, Object> searchParams) {
-		Map<String, SearchFilter> filters = Maps.newHashMap();
+    public SearchFilter(String fieldName, Operator operator, Object value) {
+        this.fieldName = fieldName;
+        this.value = value;
+        this.operator = operator;
+    }
 
-		for (Entry<String, Object> entry : searchParams.entrySet()) {
-			// 过滤掉空值
-			String key = entry.getKey();
-			Object value = entry.getValue();
-			if (StringUtils.isBlank((String) value)) {
-				continue;
-			}
+    /**
+     * searchParams中key的格式为OPERATOR_FIELDNAME
+     */
+    public static Map<String, SearchFilter> parse(Map<String, Object> searchParams) {
+        Map<String, SearchFilter> filters = Maps.newHashMap();
 
-			// 拆分operator与filedAttribute
-			String[] names = StringUtils.split(key, "_");
-			if (names.length != 2) {
-				throw new IllegalArgumentException(key + " is not a valid search filter name");
-			}
-			String filedName = names[1];
-			Operator operator = Operator.valueOf(names[0]);
+        for (Entry<String, Object> entry : searchParams.entrySet()) {
+            // 过滤掉空值
+            String key = entry.getKey();
+            Object value = entry.getValue();
+            if (StringUtils.isBlank((String) value)) {
+                continue;
+            }
 
-			// 创建searchFilter
-			SearchFilter filter = new SearchFilter(filedName, operator, value);
-			filters.put(key, filter);
-		}
+            // 拆分operator与filedAttribute
+            String[] names = StringUtils.split(key, "_");
+            if (names.length < 2 || names.length > 3) {
+                throw new IllegalArgumentException(key + " is not a valid search filter name");
+            }
+            String filedName = names[1];
+            Operator operator = Operator.valueOf(names[0]);
+            if (names.length == 3) {
+                switch (Type.valueOf(names[2])) {
+                    case DATETIME:
+                        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+                        try {
+                            value = sdf.parse(value.toString());
+                        } catch (ParseException e) {
+                            throw new IllegalArgumentException(key + " is not the time format yyyy-MM-dd HH: mm: ss");
+                        }
+                        break;
+                }
+            }
 
-		return filters;
-	}
+            // 创建searchFilter
+            SearchFilter filter = new SearchFilter(filedName, operator, value);
+            filters.put(key, filter);
+        }
+
+        return filters;
+    }
 }
