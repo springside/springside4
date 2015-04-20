3300fd6ff1504c3e4d6a50df613f548d2009c422
qijp <qijp@qijp-PC.ai.com>
2015-04-20 17:48:44 +0800

    测试空格

--- modules/utils/src/main/java/org/springside/modules/utils/Encodes.java
+++ modules/utils/src/main/java/org/springside/modules/utils/Encodes.java
@@ -17,7 +17,7 @@ import org.apache.commons.lang3.StringEscapeUtils;
 /**
  * 封装各种格式的编码解码工具类.
  * 
- * 1.Commons-Codec的 hex/base64 编码
+ * 1.Commons-Codec的 hex/base64 编码  
  * 2.自制的base62 编码
  * 3.Commons-Lang的xml/html escape
  * 4.JDK提供的URLEncoder
