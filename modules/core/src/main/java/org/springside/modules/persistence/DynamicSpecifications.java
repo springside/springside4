753843fa5f0bb8b76978d48d57c120e20b442aa0
徐稳 <uncoseason@163.com>
2015-10-16 18:50:54 +0800

    基于SearchFilter的DynamicSpecification查询添加如下支持： 1.枚举查询 2.对象级联查询

--- modules/core/src/main/java/org/springside/modules/persistence/DynamicSpecifications.java
+++ modules/core/src/main/java/org/springside/modules/persistence/DynamicSpecifications.java
@@ -8,11 +8,7 @@ package org.springside.modules.persistence;
 import java.util.Collection;
 import java.util.List;
 
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.CriteriaQuery;
-import javax.persistence.criteria.Path;
-import javax.persistence.criteria.Predicate;
-import javax.persistence.criteria.Root;
+import javax.persistence.criteria.*;
 
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.data.jpa.domain.Specification;
@@ -33,11 +29,27 @@ public class DynamicSpecifications {
 						// nested path translate, 如Task的名为"user.name"的filedName, 转换为Task.user.name属性
 						String[] names = StringUtils.split(filter.fieldName, ".");
 						Path expression = root.get(names[0]);
-						for (int i = 1; i < names.length; i++) {
-							expression = expression.get(names[i]);
-						}
+                        for (int i = 1; i < names.length; i++) {
+                            if (Collection.class.isAssignableFrom(expression.getJavaType())) {
+                                Join parentJoin = null;
+                                for (int j = 0; j < i; j++) {
+                                    if (j == 0) {
+                                        parentJoin = root.join(names[j]);
+                                    } else {
+                                        parentJoin = parentJoin.join(names[j]);
+                                    }
+                                }
+                                expression = parentJoin.get(names[i]);
+                            } else {
+                                expression = expression.get(names[i]);
+                            }
+                        }
 
-						// logic operator
+                        if (expression.getJavaType().isEnum()) {
+                            filter.value = Enum.valueOf(expression.getJavaType(), filter.value.toString());
+                        }
+
+                        // logic operator
 						switch (filter.operator) {
 						case EQ:
 							predicates.add(builder.equal(expression, filter.value));
