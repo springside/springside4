bdb1cbd5fd6be9a8f224b147c2a4afd873e788f0
calvin.xiao <calvin.xiao@vipshop.com>
2015-11-29 13:44:56 +0800

    Threads的shutdownNow直接使用Guava函数

--- modules/utils/src/main/java/org/springside/modules/utils/Threads.java
+++ modules/utils/src/main/java/org/springside/modules/utils/Threads.java
@@ -12,6 +12,7 @@ import java.util.concurrent.TimeUnit;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.google.common.util.concurrent.MoreExecutors;
 import com.google.common.util.concurrent.ThreadFactoryBuilder;
 
 /**
@@ -22,18 +23,18 @@ import com.google.common.util.concurrent.ThreadFactoryBuilder;
 public class Threads {
 
 	/**
-	 * sleep等待, 单位为毫秒.
+	 * sleep等待, 单位为毫秒. 已捕捉并处理InterruptedException.
 	 */
-	public static void sleep(long durationMillis) {
+	public static void sleep(long durationInMillis) {
 		try {
-			Thread.sleep(durationMillis);
+			Thread.sleep(durationInMillis);
 		} catch (InterruptedException e) {
 			Thread.currentThread().interrupt();
 		}
 	}
 
 	/**
-	 * sleep等待.
+	 * sleep等待. 已捕捉并处理InterruptedException.
 	 */
 	public static void sleep(long duration, TimeUnit unit) {
 		try {
@@ -44,21 +45,20 @@ public class Threads {
 	}
 
 	/**
-	 * 创建ThreadFactory，使得创建的线程有自己的名字而不是默认的"pool-x-thread-y"
+	 * 按照ExecutorService JavaDoc示例代码编写的Graceful Shutdown方法.
 	 * 
-	 * 在用threaddump查看线程时特别有用。 格式如"mythread-%d"，使用了Guava的工具类
-	 */
-	public static ThreadFactory buildJobFactory(String nameFormat) {
-		return new ThreadFactoryBuilder().setNameFormat(nameFormat).build();
-	}
-
-	/**
-	 * 创建ThreadFactory，使得创建的线程有自己的名字而不是默认的"pool-x-thread-y"
+	 * 先使用shutdown, 停止接收新任务并尝试完成所有已存在任务.
 	 * 
-	 * 在用threaddump查看线程时特别有用。 格式如"mythread-%d"，使用了Guava的工具类
+	 * 如果1/2超时时间后, 则调用shutdownNow,取消在workQueue中Pending的任务,并中断所有阻塞函数.
+	 * 
+	 * 如果1/2超时仍然超時，則強制退出.
+	 * 
+	 * 另对在shutdown时线程本身被调用中断做了处理.
+	 * 
+	 * 返回线程最后是否被中断.
 	 */
-	public static ThreadFactory buildJobFactory(String nameFormat, boolean daemon) {
-		return new ThreadFactoryBuilder().setNameFormat(nameFormat).setDaemon(daemon).build();
+	public static boolean gracefulShutdown(ExecutorService threadPool, int shutdownTimeoutInMills) {
+		return MoreExecutors.shutdownAndAwaitTermination(threadPool, shutdownTimeoutInMills, TimeUnit.MILLISECONDS);
 	}
 
 	/**
@@ -66,44 +66,34 @@ public class Threads {
 	 * 
 	 * 先使用shutdown, 停止接收新任务并尝试完成所有已存在任务.
 	 * 
-	 * 如果超时, 则调用shutdownNow,取消在workQueue中Pending的任务,并中断所有阻塞函数.
+	 * 如果1/2超时时间后, 则调用shutdownNow,取消在workQueue中Pending的任务,并中断所有阻塞函数.
 	 * 
-	 * 如果仍然超時，則強制退出. 另对在shutdown时线程本身被调用中断做了处理.
+	 * 如果1/2超时仍然超時，則強制退出.
+	 * 
+	 * 另对在shutdown时线程本身被调用中断做了处理.
+	 * 
+	 * 返回线程最后是否被中断.
 	 */
-	public static void gracefulShutdown(ExecutorService pool, int shutdownTimeout, int shutdownNowTimeout,
-			TimeUnit timeUnit) {
-		pool.shutdown(); // Disable new tasks from being submitted
-		try {
-			// Wait a while for existing tasks to terminate
-			if (!pool.awaitTermination(shutdownTimeout, timeUnit)) {
-				pool.shutdownNow(); // Cancel currently executing tasks
-				// Wait a while for tasks to respond to being cancelled
-				if (!pool.awaitTermination(shutdownNowTimeout, timeUnit)) {
-					System.err.println("Pool did not terminated");
-				}
-			}
-		} catch (InterruptedException ie) {
-			// (Re-)Cancel if current thread also interrupted
-			pool.shutdownNow();
-			// Preserve interrupt status
-			Thread.currentThread().interrupt();
-		}
+	public static boolean gracefulShutdown(ExecutorService threadPool, int shutdownTimeout, TimeUnit timeUnit) {
+		return MoreExecutors.shutdownAndAwaitTermination(threadPool, shutdownTimeout, timeUnit);
 	}
 
 	/**
-	 * 直接调用shutdownNow的方法, 有timeout控制.
+	 * 创建ThreadFactory，使得创建的线程有自己的名字而不是默认的"pool-x-thread-y"
 	 * 
-	 * 取消在workQueue中Pending的任务,并中断所有阻塞函数.
+	 * 格式如"mythread-%d"，使用了Guava的工具类
 	 */
-	public static void normalShutdown(ExecutorService pool, int timeout, TimeUnit timeUnit) {
-		try {
-			pool.shutdownNow();
-			if (!pool.awaitTermination(timeout, timeUnit)) {
-				System.err.println("Pool did not terminated");
-			}
-		} catch (InterruptedException ie) {
-			Thread.currentThread().interrupt();
-		}
+	public static ThreadFactory buildThreadFactory(String nameFormat) {
+		return new ThreadFactoryBuilder().setNameFormat(nameFormat).build();
+	}
+
+	/**
+	 * 创建ThreadFactory，使得创建的线程有自己的名字而不是默认的"pool-x-thread-y"
+	 * 
+	 * 格式如"mythread-%d"，使用了Guava的工具类
+	 */
+	public static ThreadFactory buildThreadFactory(String nameFormat, boolean daemon) {
+		return new ThreadFactoryBuilder().setNameFormat(nameFormat).setDaemon(daemon).build();
 	}
 
 	/**
@@ -124,7 +114,7 @@ public class Threads {
 			try {
 				runnable.run();
 			} catch (Throwable e) {
-				// catch any exception, because the scheduled thread will break if the exception thrown outside.
+				// catch any exception, because the scheduled thread will break if the exception thrown to outside.
 				logger.error("Unexpected error occurred in task", e);
 			}
 		}
